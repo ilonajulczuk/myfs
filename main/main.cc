68aad3bbc59d037f2541f07eb6baa14e973e38d4
Justyna Ilczuk <att@atte.ro>
2016-03-29 23:36:03 +0200

    Make templates work

--- main/main.cc
+++ main/main.cc
@@ -1,6 +1,13 @@
 #include "btree.h"
+#include "bfs.h"
+#include <string>
 #include <iostream>
 
+
+template<typename K, typename V>
+void printNode(BTree::Node<K, V>* node) {
+    std::cout << node->ToString() << std::endl;
+}
 
 int main() {
     BTree::Tree<int, int> t;
